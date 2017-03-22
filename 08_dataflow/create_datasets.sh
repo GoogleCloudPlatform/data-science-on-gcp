1ede5eea8705f43251143bae9a592dcd59ac8617
Lak Lakshmanan <vlakshmanan@google.com>
2017-03-22 07:37:37 -0700

    temporary fix: postprocessing the files to remove duplicates.

--- 08_dataflow/create_datasets.sh
+++ 08_dataflow/create_datasets.sh
@@ -6,4 +6,6 @@ cd chapter8
 
 mvn compile exec:java \
  -Dexec.mainClass=com.google.cloud.training.flights.CreateTrainingDataset9 \
-      -Dexec.args="--fullDataset=true"
+ -Dexec.args="--fullDataset=true --maxNumWorkers=50 --autoscalingAlgorithm=THROUGHPUT_BASED"
+
+# --workerMachineType=n1-highmem-8"
