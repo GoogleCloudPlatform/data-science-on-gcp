20ee1704a66c56f064d204c4745805bda2a76c53
Lak Lakshmanan <vlakshmanan@google.com>
2017-04-25 12:04:33 -0700

    prediction and reasoning about the prediction

--- 09_cloudml/call_predict_reason.py
+++ 09_cloudml/call_predict_reason.py
@@ -11,11 +11,11 @@ api = discovery.build('ml', 'v1', credentials=credentials,
 request_data = {'instances':
   [
       {
-        'dep_delay': 16.0,
-        'taxiout': 13.0,
+        'dep_delay': dep_delay,
+        'taxiout': taxiout,
         'distance': 160.0,
         'avg_dep_delay': 13.34,
-        'avg_arr_delay': 67.0,
+        'avg_arr_delay': avg_arr_delay,
         'carrier': 'AS',
         'dep_lat': 61.17,
         'dep_lon': -150.00,
@@ -24,6 +24,12 @@ request_data = {'instances':
         'origin': 'ANC',
         'dest': 'CDV'
       }
+      for dep_delay, taxiout, avg_arr_delay in
+        [[16.0, 13.0, 67.0],
+         [13.3, 13.0, 67.0], # if dep_delay was the airport mean
+         [16.0, 16.0, 67.0], # if taxiout was the global mean
+         [16.0, 13.0, 4] # if avg_arr_delay was the global mean
+        ]
   ]
 }
 
@@ -31,3 +37,6 @@ PROJECT = 'cloud-training-demos'
 parent = 'projects/%s/models/%s/versions/%s' % (PROJECT, 'flights', 'v1')
 response = api.projects().predict(body=request_data, name=parent).execute()
 print "response={0}".format(response)
+
+probs = [pred[u'probabilities'][1] for pred in response[u'predictions']]
+print "probs={0}".format(probs)
