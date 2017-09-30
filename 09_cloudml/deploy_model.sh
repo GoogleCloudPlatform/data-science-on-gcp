83c9d5abdaa80280b094f39837ee4af6bc7f2675
Lak Lakshmanan <vlakshmanan@google.com>
2017-09-29 20:49:42 -0700

    fix error message

--- 09_cloudml/deploy_model.sh
+++ 09_cloudml/deploy_model.sh
@@ -13,9 +13,10 @@ MODEL_VERSION="v1"
 
 # result of training
 MODEL_LOCATION=$(gsutil ls gs://${BUCKET}/flights/chapter9/output/export/Servo/ | tail -1)
-echo "Deleting and deploying $MODEL_NAME $MODEL_VERSION from $MODEL_LOCATION ... this will take a few minutes"
+
+echo "Deleting and deploying $MODEL_NAME $MODEL_VERSION from $MODEL_LOCATION ... this will take a few minutes ... the first two calls to delete version/model will throw ERROR if this is the first time you are deploying the model (you can ignore those)"
 
 gcloud ml-engine versions delete ${MODEL_VERSION} --model ${MODEL_NAME}
-#gcloud ml-engine models delete ${MODEL_NAME}
-#gcloud ml-engine models create ${MODEL_NAME} --regions $REGION
+gcloud ml-engine models delete ${MODEL_NAME}
+gcloud ml-engine models create ${MODEL_NAME} --regions $REGION
 gcloud ml-engine versions create ${MODEL_VERSION} --model ${MODEL_NAME} --origin ${MODEL_LOCATION}
