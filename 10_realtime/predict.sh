95453852ec1f1cc651e82d2f8793c172fddc700f
Lak Lakshmanan <vlakshmanan@google.com>
2017-10-01 11:14:35 -0700

    make bucket modifiable

--- 10_realtime/predict.sh
+++ 10_realtime/predict.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-gsutil -m rm -rf gs://cloud-training-demos-ml/flights/chapter10/output
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./predict.sh bucket-name"
+    exit
+fi
+
+BUCKET=$1
+
+gsutil -m rm -rf gs://$BUCKET/flights/chapter10/output
 bq rm -f flights.predictions
 
 cd chapter10
 
 mvn compile exec:java \
  -Dexec.mainClass=com.google.cloud.training.flights.AddRealtimePrediction \
- -Dexec.args="--realtime --speedupFactor=60 --maxNumWorkers=10 --autoscalingAlgorithm=THROUGHPUT_BASED"
+ -Dexec.args="--realtime --speedupFactor=60 --maxNumWorkers=10 --autoscalingAlgorithm=THROUGHPUT_BASED --bucket=$BUCKET"
 
--- 10_realtime/to_bq.sh
+++ 10_realtime/to_bq.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-bq load -F , flights2016.eval gs://cloud-training-demos-ml/flights/chapter10/eval/*.csv evaldata.json
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./to_bq.sh bucket-name"
+    exit
+fi
+
+BUCKET=$1
+
+bq load -F , flights2016.eval gs://$BUCKET/flights/chapter10/eval/*.csv evaldata.json
