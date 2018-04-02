bfd4a32a5b54a8bf2e0ef2a8ecc81a0f57a0fb9c
Lak Lakshmanan <vlakshmanan@google.com>
2018-04-01 17:49:07 -0700

    add project

--- 08_dataflow/create_datasets.sh
+++ 08_dataflow/create_datasets.sh
@@ -5,6 +5,7 @@ if [ "$#" -ne 2 ]; then
     exit
 fi
 
+PROJECT=$DEVSHELL_PROJECT_ID
 BUCKET=$1
 MAX_NUM_WORKERS=$2
 
@@ -14,6 +15,6 @@ cd chapter8
 
 mvn compile exec:java \
  -Dexec.mainClass=com.google.cloud.training.flights.CreateTrainingDataset \
- -Dexec.args="--bucket=$BUCKET --fullDataset=true --maxNumWorkers=$MAX_NUM_WORKERS --autoscalingAlgorithm=THROUGHPUT_BASED"
+ -Dexec.args="--project=$PROJECT --bucket=$BUCKET --fullDataset=true --maxNumWorkers=$MAX_NUM_WORKERS --autoscalingAlgorithm=THROUGHPUT_BASED"
 
 # --workerMachineType=n1-highmem-8"
