989e44df196e873d9446bcd1fa0495d26f322506
Miki <myoshimu@users.noreply.github.com>
2019-01-29 04:47:16 +0900

    Add python client libraries upgrade
    Add python client libraries upgrade

--- 04_streaming/simulate/install_packages.sh
+++ 04_streaming/simulate/install_packages.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo pip install timezonefinder pytz apache-beam 'apache-beam[gcp]'
+sudo pip install --upgrade google-cloud-bigquery
+sudo pip install --upgrade google-cloud-pubsub
 #sudo pip install --force six==1.10  # downgrade as 1.11 breaks apitools
