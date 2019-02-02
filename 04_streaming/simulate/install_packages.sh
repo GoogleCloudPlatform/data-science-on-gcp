7eb482cccee91d37d87420fe98b3c69b12bb8281
A4q5PMrDW1d+vkfDYmdoLZOcKFKhZHgRZazKmBiqgKE= <lak@vlakshman.com>
2019-02-02 00:07:11 -0800

    Update install_packages.sh

--- 04_streaming/simulate/install_packages.sh
+++ 04_streaming/simulate/install_packages.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo pip install timezonefinder pytz apache-beam 'apache-beam[gcp]'
-#sudo pip install --force six==1.10  # downgrade as 1.11 breaks apitools
+sudo pip install --upgrade timezonefinder pytz apache-beam 'apache-beam[gcp]'
