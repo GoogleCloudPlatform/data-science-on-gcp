761ef0673dfcdb1397e020ba2e96163ec311a4e7
Miki <myoshimu@users.noreply.github.com>
2019-02-22 13:41:44 +0900

    Change the directory to $HOME
    Change the directory to $HOME

--- 06_dataproc/install_on_cluster.sh
+++ 06_dataproc/install_on_cluster.sh
@@ -6,9 +6,8 @@ apt-get install -y python-pip
 pip install --upgrade google-api-python-client
 
 # git clone on Master
-USER=CHANGE_TO_USER_NAME   # change this ...
 ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
 if [[ "${ROLE}" == 'Master' ]]; then
-  cd home/$USER
+  cd $HOME
   git clone https://github.com/GoogleCloudPlatform/data-science-on-gcp
 fi
