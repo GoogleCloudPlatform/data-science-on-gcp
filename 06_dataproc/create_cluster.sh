02825fbb107cce92690563aa6e8c46b08ccc534a
Valliappa (Lak) Lakshmanan <lak@vlakshman.com>
2021-02-23 22:02:04 -0800

    dataproc now wants REGION
    also now GA

--- 06_dataproc/create_cluster.sh
+++ 06_dataproc/create_cluster.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "Usage: ./create_cluster.sh  bucket-name  zone"
+    echo "Usage: ./create_cluster.sh  bucket-name  region"
     exit
 fi
 
 PROJECT=$(gcloud config get-value project)
 BUCKET=$1
-ZONE=$2
+REGION=$2
 INSTALL=gs://$BUCKET/flights/dataproc/install_on_cluster.sh
 
 # upload install file
@@ -15,7 +15,7 @@ sed "s/CHANGE_TO_USER_NAME/$USER/g" install_on_cluster.sh > /tmp/install_on_clus
 gsutil cp /tmp/install_on_cluster.sh $INSTALL
 
 # create cluster
-gcloud beta dataproc clusters create \
+gcloud dataproc clusters create \
    --num-workers=2 \
    --scopes=cloud-platform \
    --worker-machine-type=n1-standard-4 \
@@ -23,6 +23,6 @@ gcloud beta dataproc clusters create \
    --image-version=1.4 \
    --enable-component-gateway \
    --optional-components=ANACONDA,JUPYTER \
-   --zone=$ZONE \
+   --region=$REGION \
    --initialization-actions=$INSTALL \
    ch6cluster
