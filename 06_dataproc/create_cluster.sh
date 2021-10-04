e44fb3592b8d0e7ea6664af8111c5f47e1f08c69
Lakshmanan, V <lakshmanok@gmail.com>
2021-10-04 16:39:12 -0700

    updating ...

--- 06_dataproc/create_cluster.sh
+++ 06_dataproc/create_cluster.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: ./create_cluster.sh  bucket-name  region"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: ./create_cluster.sh  bucket-name  region  user_email"
     exit
 fi
 
 PROJECT=$(gcloud config get-value project)
 BUCKET=$1
 REGION=$2
+EMAIL=$3
 INSTALL=gs://$BUCKET/flights/dataproc/install_on_cluster.sh
 
 # upload install file
-sed "s/CHANGE_TO_USER_NAME/$USER/g" install_on_cluster.sh > /tmp/install_on_cluster.sh
+# sed "s/CHANGE_TO_USER_NAME/$USER/g" install_on_cluster.sh > /tmp/install_on_cluster.sh
+sed "s/CHANGE_TO_USER_NAME/dataproc/g" install_on_cluster.sh > /tmp/install_on_cluster.sh
 gsutil cp /tmp/install_on_cluster.sh $INSTALL
 
 # create cluster
-gcloud dataproc clusters create \
-   --num-workers=2 \
-   --scopes=cloud-platform \
-   --worker-machine-type=n1-standard-4 \
-   --master-machine-type=n1-standard-4 \
-   --image-version=1.4 \
-   --enable-component-gateway \
-   --optional-components=ANACONDA,JUPYTER \
-   --region=$REGION \
-   --initialization-actions=$INSTALL \
-   ch6cluster
+gcloud dataproc clusters create ch6cluster \
+  --enable-component-gateway \
+  --region ${REGION} --zone ${REGION}-a \
+  --master-machine-type n1-standard-4 \
+  --master-boot-disk-size 500 --num-workers 2 \
+  --worker-machine-type n1-standard-4 \
+  --worker-boot-disk-size 500 \
+  --properties dataproc:dataproc.personal-auth.user=$EMAIL \
+  --optional-components JUPYTER --project $PROJECT \
+  --initialization-actions=$INSTALL \
+  --scopes https://www.googleapis.com/auth/cloud-platform
--- 06_dataproc/install_on_cluster.sh
+++ 06_dataproc/install_on_cluster.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # install Google Python client on all nodes
-apt-get update
+apt-get -y update
+apt-get install python-dev
 apt-get install -y python-pip
 pip install --upgrade google-api-python-client
 
 # git clone on Master
-USER=CHANGE_TO_USER_NAME   # change this ...
+USER=CHANGE_TO_USER_NAME # the username that dataproc runs as
 ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
 if [[ "${ROLE}" == 'Master' ]]; then
   cd home/$USER
   git clone https://github.com/GoogleCloudPlatform/data-science-on-gcp
+  chown -R $USER data-science-on-gcp
 fi
