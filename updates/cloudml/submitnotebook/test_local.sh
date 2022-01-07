c87feba1fe6b2e35bde2b42267facc7eb13f0309
Lakshmanan, V <lakshmanok@gmail.com>
2022-01-06 17:26:27 -0800

    updates folded into main repo

--- updates/cloudml/submitnotebook/test_local.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run gcr.io/cloud-training-demos/submitnotebook:latest gs://cloud-training-demos-ml/flights/notebook/flights_small.ipynb gs://cloud-training-demos-ml/flights/notebook/flights_small_kfp.ipynb gs://cloud-training-demos-ml/flights/notebook/params.yaml
