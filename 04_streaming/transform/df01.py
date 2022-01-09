79f7a86cc5b93d79f80d86833cf3a0973a756254
Colin Dietrich <repos@wildjuniper.com>
2022-01-08 22:24:15 -0800

    Apply PEP8 reformat to indents & return statements

--- 04_streaming/transform/df01.py
+++ 04_streaming/transform/df01.py
@@ -18,15 +18,14 @@ import apache_beam as beam
 import csv
 
 if __name__ == '__main__':
-   with beam.Pipeline('DirectRunner') as pipeline:
+    with beam.Pipeline('DirectRunner') as pipeline:
+        airports = (pipeline
+                    | beam.io.ReadFromText('airports.csv.gz')
+                    | beam.Map(lambda line: next(csv.reader([line])))
+                    | beam.Map(lambda fields: (fields[0], (fields[21], fields[26])))
+                    )
 
-      airports = (pipeline
-         | beam.io.ReadFromText('airports.csv.gz')
-         | beam.Map(lambda line: next(csv.reader([line])))
-         | beam.Map(lambda fields: (fields[0], (fields[21], fields[26])))
-      )
-
-      (airports 
-         | beam.Map(lambda airport_data: '{},{}'.format(airport_data[0], ','.join(airport_data[1])) )
+        (airports
+         | beam.Map(lambda airport_data: '{},{}'.format(airport_data[0], ','.join(airport_data[1])))
          | beam.io.WriteToText('extracted_airports')
-      )
+         )
