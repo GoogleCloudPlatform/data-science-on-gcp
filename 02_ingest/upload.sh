e86929cda81cc3153d3f3e7274662988bb81d0d8
SharanyaNallamothu <97906958+SharanyaNallamothu@users.noreply.github.com>
2022-02-08 09:25:38 -0500

    Update upload.sh

--- 02_ingest/upload.sh
+++ 02_ingest/upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export BUCKET=${BUCKET:=cloud-training-demos-ml}
+export BUCKET=${BUCKET:=uncc6155-spring2022-sharanya}
 echo "Uploading to bucket $BUCKET..."
 gsutil -m cp *.csv gs://$BUCKET/flights/raw
 #gsutil -m acl ch -R -g google.com:R gs://$BUCKET/flights/raw
